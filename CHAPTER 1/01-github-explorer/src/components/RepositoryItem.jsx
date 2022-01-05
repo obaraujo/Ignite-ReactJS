1ac62205742a74f994c9132e6e8de1766fda41f9
Vinícius Araújo <viniciusaraujotech@gmail.com>
2022-01-05 15:54:04 -0300

    Modulo 3 concluido

--- CHAPTER 1/01-github-explorer/src/components/RepositoryItem.jsx
+++ CHAPTER 1/01-github-explorer/src/components/RepositoryItem.jsx
@@ -3,7 +3,7 @@ export function RepositoryItem(props) {
     <li>
       <strong>{props.repository?.name ?? "Default"}</strong>
       <p>{props.repository?.description ?? "Default"}</p>
-      <a href="">{props.repository?.link ?? "Default"}</a>
+      <a href={props.repository?.html_url}>Acesse</a>
     </li>
   );
 }
